20a010692e97982fc1d2f64c63334a6dcf0644b2
CalebLGreen <167186139+CalebLGreen@users.noreply.github.com>
2024-08-26 14:05:54 +0100

    made the build function more modular
    build function is now more modular, can accept a thing to build so that the function doesn't need to be re-done for every object.
    Tried to make a move object function. Currently not working so commented it out

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -20,13 +20,13 @@ var tower_segment_height = 3.9
 var timer = false
 
 # Buildables
-var ghost_table = null
-
+var temp_object = null
 
 # Shop variables
 var in_build_menu : bool = false
-var building_table : bool = false
-
+var building_table : Dictionary = {'value' : false}
+var moving_object : Dictionary = {'value' : false}
+var selected_object = null
 
 func _ready() -> void:
 	# Reset camera
@@ -61,69 +61,77 @@ func handle_mouse_controls(delta) -> void:
 	#print(ray.collision_mask)
 	# Store values from collision
 	var ray_result : Dictionary = space_state.intersect_ray(ray)
-	if timer:
-		pass
-	elif not timer:
-		timer = get_tree().create_timer(1.0)
-		await timer.timeout
-		print(origin, ", ", end, " ", ray_result)
-		timer = false
+	# DEBUG 1 per second timer print
+	#if timer:
+		#pass
+	#elif not timer:
+		#timer = get_tree().create_timer(1.0)
+		#await timer.timeout
+		#print(origin, ", ", end, " ", ray_result)
+		#timer = false
 	# Now if building
 	if in_build_menu:
 		# Check if the table is being built
-		if building_table:
-			build_table(ray_result, delta)
+		if building_table.value:
+			build(table, "Table", building_table, ray_result, delta)
+		#if moving_object.value:
+			#move(ray_result, moving_object, delta)
 
 
-func build_table(ray_result : Dictionary, delta : float):
+func build(object : PackedScene, object_name : String, building_object : Dictionary, ray_result: Dictionary, delta: float) -> void:
 	# stop the user changing floors
 	camera.is_movement_locked = true
+	
 	# DEBUG print
 	#print("BUILDING")
 	# Check whether the ray has collided with anything, i.e. the floor
 	if ray_result.size() > 0:
 		# get the current floor, used for collisions and naming
 		var curr_floor = get_current_floor()
-		# if a ghostly version of the table doesn't exist, create it
-		if ghost_table == null:
-			ghost_table = table.instantiate()
-			get_node("Tower/%s" % [curr_floor]).add_child(ghost_table)
-			ghost_table.visible = true
-		
+		# if a ghostly version of the object doesn't exist, create it
+		if temp_object == null:
+
+			temp_object = object.instantiate()
+			get_node("Tower/%s" % [curr_floor]).add_child(temp_object)
+			temp_object.visible = true
+			# Add the object to the correct groups
+			
 		# Cancel placement
 		if Input.is_action_just_pressed("cancel"):
-			ghost_table.queue_free()
-			building_table = false
+			temp_object.queue_free()
+			building_object.value = false
 			camera.is_movement_locked = false
 			UI.toggle_shop()
 		
-		# If the ghostly table does already exist continue
-		if ghost_table:
-			# Change the collision layer of the table
+		# If the ghostly object does already exist continue
+		if temp_object:
+			# Change the collision layer of the object
 			# The mask layer is still one so it'll collide with walls and floors
-			# This ensures the table doesn't collide with the ray cast
-			ghost_table.get_node("Table").collision_layer = 2
+			# This ensures the object doesn't collide with the ray cast
+			# Important that the string name in the scene is correct
+			temp_object.get_node("%s" % [object_name]).collision_layer = 2
 			
-			# Get the mesh instance node from the table, used to switch it between being green, red or complete
-			var mesh_instance = ghost_table.get_node("Table/Table_Mesh")
+			# Get the mesh instance node from the object, used to switch it between being green, red or complete
+			# Be consistent with the nomenclature of child nodes in the scenes to following the same format
+			var mesh_instance = temp_object.get_node("%s/%s_Mesh" % [object_name, object_name])
 			# Get the interesction of the ray and where it collides
-			# Set the the table position to hover slightly below this point, due to my silly blender skills
+			# Set the the object position to hover slightly below this point, due to my silly blender skills
 			var intersection_point : Vector3 = ray_result.get("position")
 			
 			var current_floor_number = get_current_floor_num()
 			var offset = -0.48 - (tower_segment_height * get_current_floor_num() - 1)
-			ghost_table.position = intersection_point + Vector3(0, offset, 0)
-			prints(current_floor_number, offset, intersection_point)
+			temp_object.position = intersection_point + Vector3(0, offset, 0)
+			#prints(current_floor_number, offset, intersection_point)
 			# Allow for rotations - simple enough
 			if Input.is_action_pressed("rotate_left"):
-				ghost_table.rotation.y += delta * rotate_speed
+				temp_object.rotation.y += delta * rotate_speed
 			elif Input.is_action_pressed("rotate_right"):
-				ghost_table.rotation.y -= delta * rotate_speed
+				temp_object.rotation.y -= delta * rotate_speed
 			
 			# Get the Area3D for collisions with self
-			var area = ghost_table.get_node("Table/Table_Area")
+			var area = temp_object.get_node("%s/%s_Area" % [object_name, object_name])
 			if area and area is Area3D:
-				area.global_transform.origin = ghost_table.global_transform.origin
+				area.global_transform.origin = temp_object.global_transform.origin
 				
 				# Check for overlaps, currently can be placed on the wrong floor, must fix
 				var overlaps = area.get_overlapping_bodies()
@@ -134,23 +142,65 @@ func build_table(ray_result : Dictionary, delta : float):
 						valid_overlaps.append(body)
 				# See if there are any overlaps, in which case make the material red
 				# and it cannot be placed
-				if overlaps.size() > 0 or ghost_table.global_position.y < (camera.position.y - 1.5) or ghost_table.global_position.y > (camera.position.y-1):
-					#print(ghost_table.global_position.y, " ", camera.position.y)
+				if overlaps.size() > 0 or temp_object.global_position.y < (camera.position.y - 1.5) or temp_object.global_position.y > (camera.position.y-1):
+					#print(temp_object.global_position.y, " ", camera.position.y)
 					mesh_instance.set_surface_override_material(0, load("res://Models/red.tres"))
 				# If no overlaps then we are good to go
 				else:
 					mesh_instance.set_surface_override_material(0, load("res://Models/green.tres"))
 					if Input.is_action_just_pressed("left_click"):
 						# slightly lower the position to compensate for the slight lift
-						ghost_table.position -= Vector3(0, 0.02, 0)
+						temp_object.position -= Vector3(0, 0.02, 0)
 						# Change the collision layer back to 1
-						ghost_table.get_node("Table").collision_layer = 1
+						temp_object.get_node("%s" % [object_name]).collision_layer = 1
 						# Reset it back to default textures
 						mesh_instance.set_surface_override_material(0, null)
-						# This table now no longer exists as a variable
-						ghost_table = null
-						building_table = false
+						# This object now no longer exists as a variable
+						#print(temp_object.get_groups())
+						temp_object = null
+						building_object.value = false
 						camera.is_movement_locked = false
+
+## Function to move objects on a floor
+#func move(ray_result : Dictionary, moving_object : Dictionary, delta : float) -> void:
+	#camera.is_movement_locked = true
+	#if ray_result.size() > 0:
+		## get the current floor, used for collisions and naming
+		#var curr_floor = get_current_floor()
+		## if a ghostly version of the object doesn't exist, create it
+		#if Input.is_action_just_pressed("left_click"):
+			#if selected_object:
+				#move_object(ray_result, moving_object, delta)
+			#else:
+				#select_object(ray_result)
+		#elif Input.is_action_just_pressed("cancel"):
+			#selected_object = null
+			#moving_object.value = false
+#
+#func select_object(ray_result : Dictionary) -> void:
+	#var collider = ray_result.get("collider")
+	#if collider and collider is Node3D:
+		#selected_object = collider
+	#else:
+		#print("Object not selectable")
+
+
+func move_object(ray_result : Dictionary, moving_object : Dictionary, delta : float) -> void:
+	if selected_object:
+		var interesection_point : Vector3 = ray_result.get("position")
+		selected_object.position = interesection_point + Vector3(0, -3.38, 0)
+		
+		# Allow for rotations
+		if Input.is_action_pressed("rotate_left"):
+			selected_object.rotation.y += delta * rotate_speed
+		elif Input.is_action_pressed("rotate_right"):
+			selected_object.rotation.y -= delta * rotate_speed
+
+		# Finalize the move on left click
+		if Input.is_action_just_pressed("left_click"):
+			print("Object moved to: ", selected_object.position)
+			selected_object = null  # Deselect after moving
+			moving_object.value = false
 
 # Function to check whether the floor is the current floor
 func is_floor(body: Node3D, current_floor) -> bool:
@@ -212,4 +262,9 @@ func get_current_floor_num() -> int:
 
 # for when the button to buy a table has been pressed
 func _on_floor_add_table_pressed() -> void:
-	building_table = true
+	building_table.value = true
+
+
+func _on_move_objects_pressed() -> void:
+	#moving_object.value = true
+	pass
